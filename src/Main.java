006d11fd6254f5a69de1b665ef1e5dd3555a58ec
abaz07 <abazov.islam2017@yandex.ru>
2024-01-22 01:19:03 +0300

    homework7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        System.out.println("Задача 1");
+        int sum1 = 15000;
+        int totalAmount = 0;
+        int months = 0;
+        while (totalAmount < 2_459_000) {
+            months++;
+            totalAmount = totalAmount + totalAmount/100;
+            totalAmount = totalAmount + sum1;
+            System.out.println("Месяц " + months + ", сумма накопленний равна " + totalAmount + " рублей");
+        }
+        // Задача 2
+        System.out.println("Задача 2");
+        int number = 0;
+        while (number < 10){
+            number++;
+            System.out.print( number );
+        }
+        System.out.println();
+        for(int number1 = 10; number1 > 0; number1--){
+            System.out.print(number1);
+        }
+        // Задача 3
+        System.out.println("Задача 3");
+        int population = 12_000_000;
+        int growthInOneYear = population/1000 * (17-8);
+        for(int year = 2023; year < 2034; year++){
+            population = population + growthInOneYear;
+            System.out.println("Год " + year + " численность населения составляет " + population);
+        }
+        // Задача 4
+        System.out.println("Задача 4");
+
     }
 }
